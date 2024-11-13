3e2fbf72cc36649a47e9c4cee522ca8c93511e46
Rishi <rishilord2@gmail.com>
2024-11-13 19:52:35 +0530

    chap 1

---
+++ Chap 1/variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int number = 25;
+    int star = "*";
+    int age = 25;
+    float pi = 3.14;
+
+    int a = 30;
+    int A = 40;
+    // both are diff 
+    return 0;
+}
