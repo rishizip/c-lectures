2f933a69f3e042b1d553a64b2b06f2fb3b38cfe4
Rishi <rishilord2@gmail.com>
2024-11-14 10:31:50 +0530

    add Chap 2

---
+++ Chap 2/intruction.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    // type declaration instruction
+    // int a = 22;
+    // int b = a;
+    // int c = b * 6;
+    // int d = 1, e;
+    // int old_age = 22, years = 2;
+    // int new_age = old_age + years;
+    // int x,y,z;
+    // x = y = z = 4;
+    // return 0;
+
+    // arithmetic instruction
+
+
+    // Addition
+        int d = 5, e = 2;
+
+    // Addition
+    int sum = d + e;
+    printf("Sum: %d\n", sum);
+
+    // Subtraction
+    int difference = d - e;
+    printf("Difference: %d\n", difference);
+
+    // Multiplication
+    int product = d * e;
+    printf("Product: %d\n", product);
+
+    // Division
+    int quotient = d / e;
+    printf("Quotient: %d\n", quotient);
+
+    // Exponentiation
+    double power = pow(d, e);
+    printf("Power: %.2f\n", power);
+
+return 0;
+}
