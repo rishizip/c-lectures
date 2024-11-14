7cb03fbd93cd9d5aa0f8c21a930ec84b5932410d
Rishi <rishilord2@gmail.com>
2024-11-14 19:35:41 +0530

    add Chap 2

--- Chap 2/2-type_converstion.c
+++ Chap 2/2-type_converstion.c
@@ -2,6 +2,11 @@
 #include<stdio.h>
 int main()
 {
-    printf("%f \n",4/2.0);
-    return 0;
+    // done by coppiler
+    // printf("%f \n",4/2.0);
+    // return 0;
+
+    // explicit -> done by us
+    int a = (int) 99.99;
+    printf("%d \n",a);
 }
---
+++ Chap 2/p1.c
@@ -0,0 +1,8 @@
+// convert a = 1.99 to integer
+#include<stdio.h>
+int main()
+{
+    int a = 1.9999;
+    printf("%d",a);
+    return 0;
+}
