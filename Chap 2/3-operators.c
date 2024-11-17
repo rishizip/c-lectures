b76a61c6b097592bed3e79d01d1ac4574b89f0a4
Rishi <rishilord2@gmail.com>
2024-11-17 09:22:57 +0530

    add Chap 2

---
+++ Chap 2/3-operators.c
@@ -0,0 +1,30 @@
+// operators in c
+// a) Arithmetic - alr done
+
+// b) Relational -> ==, >, >=, <, <= 
+#include<stdio.h>
+int main()
+{
+    printf("ans = %d\n",4==4);
+    // true -> 1
+
+    printf("ans = %d\n",4==3);
+    // false -> 0
+
+    printf("ans = %d\n",3>3);
+
+    printf("ans = %d\n",3<3);
+
+    printf("ans = %d\n",3>=3);
+
+    printf("ans = %d\n",3<=3);
+    return 0;
+}
+
+// c) Logical
+
+// d) Bitwise
+
+// e) Assignment
+
+// f) Ternary
