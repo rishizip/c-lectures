32342a842a1354aea7d8e24f4188faf298bbef2f
Rishi <rishilord2@gmail.com>
2024-11-20 18:37:39 +0530

    add lectures

--- Chap 2/3-operators.c
+++ Chap 2/3-operators.c
@@ -5,26 +5,41 @@
 #include<stdio.h>
 int main()
 {
-    printf("ans = %d\n",4==4);
+    // printf("ans = %d\n",4==4);
     // true -> 1
 
-    printf("ans = %d\n",4==3);
+    // printf("ans = %d\n",4==3);
     // false -> 0
 
-    printf("ans = %d\n",3>3);
+    // printf("ans = %d\n",3>3);
 
-    printf("ans = %d\n",3<3);
+    // printf("ans = %d\n",3<3);
 
-    printf("ans = %d\n",3>=3);
+    // printf("ans = %d\n",3>=3);
 
-    printf("ans = %d\n",3<=3);
-    return 0;
-}
+    // printf("ans = %d\n",3<=3);
+
+    // printf("ans = %d\n",3!=3);
+
+    // printf("ans = %d",3==3);
 
 // c) Logical
 
+    int a,b;
+    printf("Enter 1st number : ");
+    scanf("%d",&a);
+    printf("Enter 2nd number : ");
+    scanf("%d",&b);
+    printf("and = %d\n",(a > 100)&&(b > 100));
+    printf("or = %d\n",(a > 100)&&(b > 100));
+    printf("not = %d\n",!(a>100));
+
+    
 // d) Bitwise
 
 // e) Assignment
 
 // f) Ternary
+
+return 0;
+}
