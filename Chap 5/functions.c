36a3ea2b702529a3e9aa7286056d5c91ba5aa847
Rishi <rishilord2@gmail.com>
2024-11-24 20:08:19 +0530

    add lectures

--- Chap 5/functions.c
+++ Chap 5/functions.c
@@ -1,9 +1,57 @@
-#include<stdio.h>
-void PrintHello() // function defination
-{
-    printf("Hello");
+#include <stdio.h>
+
+// Function prototypes
+void printMenu();
+void performOperation(int, int, char);
+
+int main() {
+    int num1, num2;
+    char choice;
+
+    printf("Enter 1st number : ");
+    scanf("%d", &num1);
+
+    printf("Enter 2nd number : ");
+    scanf("%d", &num2);
+
+    printMenu();
+    printf("Enter choice : ");
+    scanf(" %c", &choice); // Note the space before %c
+
+    performOperation(num1, num2, choice);
+
+    return 0;
 }
 
-int main(){
-    PrintHello(); // funcn call 
+void printMenu() {
+    printf("Choose an operation:\n");
+    printf("+ for addition\n");
+    printf("- for subtraction\n");
+    printf("* for multiplication\n");
+    printf("/ for division\n");
+}
+
+void performOperation(int num1, int num2, char choice) {
+    switch (choice) {
+        case '+':
+            printf("Sum = %d\n", num1 + num2);
+            break;
+        case '-':
+            printf("Difference = %d\n", num1 - num2);
+            break;
+        case '*':
+            printf("Product = %d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("Quotient = %d\n", num1 / num2);
+            } else {
+                printf("Cannot divide by zero!\n");
+            }
+            break;
+        default:
+            printf("Invalid choice!\n");
+            break;
+
+}
 }
