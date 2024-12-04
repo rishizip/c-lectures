1ec2f02c3eaf76339325d59267eb6a76f0997149
Rishi <rishilord2@gmail.com>
2024-12-04 20:20:16 +0530

    add lectures

--- Chap 6/02-pointer.c
+++ Chap 6/02-pointer.c
@@ -4,5 +4,9 @@ int main()
     float price = 100;
     float *ptr = &price;
     float **pptr = &ptr;
+
+    printf("price = %d\n",ptr);
+    printf("price = %d\n",*ptr);
+    printf("price = %d\n",**pptr);
     return 0;
 }
